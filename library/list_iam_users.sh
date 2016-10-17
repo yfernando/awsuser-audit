91b787b4cfbce62c41913cacb73719740ad961fd
Yohan Fernando <yohan.fernando@mnscorp.net>
2016-10-17 16:48:42 +0100

    Initial commit

---
+++ library/list_iam_users.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source $1
+
+export AWS_ACCESS_KEY_ID=${aws_access_key}
+export AWS_SECRET_ACCESS_KEY=${aws_secret_key}
+export AWS_SESSION_TOKEN=${security_token}
+
+aws iam list-users
